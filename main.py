4fa26ce7f09de7f868809c80512362b078291595
Ben <quillaja@hotmail.com>
2025-12-14 15:17:08 -0800

    abandon mem profiling, use time profiling

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-from memory_profiler import profile
+import random
+import traceback
+import tracemalloc
+from contextlib import contextmanager
+from cProfile import Profile
+from typing import Any, Generator, final
 
 
 # fmt:off
@@ -9,7 +14,6 @@ def D()->str: return "D"
 # fmt:on
 
 
-@profile
 def if_else(n: int) -> str:
     if n == 0:
         return A()
@@ -21,7 +25,6 @@ def if_else(n: int) -> str:
         return D()
 
 
-@profile
 def match_stmt(n: int) -> str:
     match n:
         case 0:
@@ -34,7 +37,6 @@ def match_stmt(n: int) -> str:
             return D()
 
 
-@profile
 def dict_prop(n: int) -> str:
     return dict_prop._funcs.get(n, D)()
 
@@ -46,7 +48,6 @@ dict_prop._funcs = {
 }
 
 
-@profile
 def dict_var(n: int) -> str:
     funcs = {
         0: A,
@@ -56,20 +57,54 @@ def dict_var(n: int) -> str:
     return funcs.get(n, D)()
 
 
-@profile
-def main():
-    n = 1
-    if_else(n)
-    match_stmt(n)
-    dict_prop(n)
-    dict_var(n)
+snapshots: dict[str, list[tracemalloc.StatisticDiff]] = {}
 
-    # import random
-    # nums = random.choices(range(5), k=10_000)
+
+@contextmanager
+def trace(name: str) -> Generator[None, None, None]:
+    before = tracemalloc.take_snapshot()
+    try:
+        yield
+    finally:
+        after = tracemalloc.take_snapshot()
+        snapshots[name] = after.compare_to(before, "lineno")
+
+
+def print_snapshots():
+    for name, stats in snapshots.items():
+        print()
+        print(name)
+        stats = [s for s in stats if "main.py" in str(s.traceback)]
+        for line in stats:
+            print(line)
+
+
+def main():
+    # tracemalloc.start()
+
+    funcs = [if_else, match_stmt, dict_prop, dict_var]
+    nums = random.choices(range(5), k=500_000)
+    for i in range(16):
+        print(f"-- round {i} --")
+        random.shuffle(funcs)
+        for f in funcs:
+            print(f.__name__)
+            for n in nums:
+                f(n)
+
     # for f in (if_else, match_stmt, dict_prop, dict_var):
-    #     for n in nums:
-    #         f(n)
+    #     # with trace(f.__name__):
+    #     before = tracemalloc.take_snapshot()
+    #     f(1)
+    #     after = tracemalloc.take_snapshot()
+    #     snapshots[f.__name__] = after.compare_to(before, "lineno")
+
+    # print_snapshots()
 
 
 if __name__ == "__main__":
+    p = Profile()
+    p.enable()
     main()
+    p.disable()
+    p.dump_stats("stats.prof")
